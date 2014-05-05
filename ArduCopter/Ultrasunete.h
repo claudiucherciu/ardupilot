1ea2c580ec99d3a253c0da459f92edfaf6ebd11d
Claudiu <cherciu_claudiu@yahoo.com>
2014-05-05 12:36:34 +0300

    Implements Ultrasonic sensors read

--- ArduCopter/Ultrasunete.h
+++ ArduCopter/Ultrasunete.h
@@ -1,1 +1,55 @@
 // ultrasunete.h
+/* ATMEGA2560 AnalogIN pin mappings:
+ *
+ * PF0 ( ADC0 )	                Analog pin 0
+ * PF1 ( ADC1 )	                Analog pin 1
+ * PF2 ( ADC2 )	                Analog pin 2
+ * PF3 ( ADC3 )	                Analog pin 3
+ * PF4 ( ADC4/TMK )	        Analog pin 4
+ * PF5 ( ADC5/TMS )	        Analog pin 5
+ * PF6 ( ADC6 )	                Analog pin 6
+ * PF7 ( ADC7 )	                Analog pin 7
+ * PK0 ( ADC8/PCINT16 )      	Analog pin 8
+ * PK1 ( ADC9/PCINT17 )	        Analog pin 9
+ * PK0 ( ADC8/PCINT16 )      	Analog pin 8
+ * PK1 ( ADC9/PCINT17 )	        Analog pin 9
+ * PK2 ( ADC10/PCINT18 )	Analog pin 10
+ * PK3 ( ADC11/PCINT19 )	Analog pin 11
+ * PK4 ( ADC12/PCINT20 )	Analog pin 12
+ * PK5 ( ADC13/PCINT21 )	Analog pin 13
+ * PK6 ( ADC14/PCINT22 )	Analog pin 14
+ * PK7 ( ADC15/PCINT23 )	Analog pin 15  
+ *
+ * Use these registers:
+ * PCICR - bit 2 (PCIE2) to enable PCINT23:16
+ * PCMSK2 - to select which pins have interrupts enabled
+ * 
+ */
+
+#include<avr/interrupt.h>
+
+//PINS
+//Do not change ECHO port!
+#define SENZOR_ECHO_PIN      PINK
+#define SENZOR_ECHO_DDR      DDRK
+#define SENZOR_FATA_ECHO     0
+#define SENZOR_SPATE_ECHO    1
+#define SENZOR_STANGA_ECHO   2
+#define SENZOR_DREAPTA_ECHO  3
+
+
+#define SENZOR_TRIG_PORT     PORTF
+#define SENZOR_TRIG_DDR      DDRF
+#define SENZOR_FATA_TRIG     0
+#define SENZOR_SPATE_TRIG    1
+#define SENZOR_STANGA_TRIG   2
+#define SENZOR_DREAPTA_TRIG  3
+
+#define SENZOR_MAX_RANGE    400 //cm
+#define SENZOR_MIN_RANGE      2 //cm
+
+
+//4 sensors {Fata, spate, stanga, dreapta}
+uint16_t distante[4] = {SENZOR_MAX_RANGE, SENZOR_MAX_RANGE, SENZOR_MAX_RANGE, SENZOR_MAX_RANGE};
+volatile unsigned long distante_start_millis[4] = {0};
+volatile unsigned long distante_end_millis[4] = {0};
--- ArduCopter/Ultrasunete.ino
+++ ArduCopter/Ultrasunete.ino
@@ -1,1 +1,106 @@
 // Ultrasunete.ino
+
+//handy macros 
+#define output_low(port,pin) port &= ~(1<<pin) 
+#define output_high(port,pin) port |= (1<<pin) 
+#define set_input(portdir,pin) portdir &= ~(1<<pin) 
+#define set_output(portdir,pin) portdir |= (1<<pin) 
+#define read_pin(port, pin) port & (1<<pin) 
+
+
+void ultrasunete_init() {
+
+  //set TRIG pins as output
+  set_output(SENZOR_TRIG_DDR, SENZOR_FATA_TRIG);
+  set_output(SENZOR_TRIG_DDR, SENZOR_SPATE_TRIG);
+  set_output(SENZOR_TRIG_DDR, SENZOR_STANGA_TRIG);
+  set_output(SENZOR_TRIG_DDR, SENZOR_DREAPTA_TRIG);
+  //output low all TRIG pins
+  SENZOR_TRIG_PORT &= ~((1<<SENZOR_FATA_TRIG) | (1<<SENZOR_SPATE_TRIG) | (1<<SENZOR_STANGA_TRIG) | (1<<SENZOR_DREAPTA_TRIG));
+
+  //enable PCINT for ECHO pins
+  PCMSK2 |= ( (1<<SENZOR_FATA_ECHO) | (1<<SENZOR_SPATE_ECHO) | (1<<SENZOR_STANGA_ECHO) | (1<<SENZOR_DREAPTA_ECHO));
+  //PCICR  |=   (1<<PCIE2); //enable ISR
+}
+
+
+void ultrasunete_50Hz() {
+
+  static unsigned int state = 0;
+  switch(state) {
+    case 0:
+            //disable ISR to avoid timing updates and race conditions
+            PCICR  &=   ~(1<<PCIE2); 
+            
+            //output high all TRIG pins;
+            SENZOR_TRIG_PORT |= ((1<<SENZOR_FATA_TRIG) | (1<<SENZOR_SPATE_TRIG) | (1<<SENZOR_STANGA_TRIG) | (1<<SENZOR_DREAPTA_TRIG));
+            
+            //Compute last distances
+            for(int i = 0; i < 4; i++) {
+              unsigned long timp = distante_end_millis[i] - distante_start_millis[i];
+              if (timp > 0) {
+                timp = timp / 58;
+                if ((timp > SENZOR_MIN_RANGE) && (timp < SENZOR_MAX_RANGE)) {
+                  timp = distante[i];
+                }
+              }
+            }
+            
+            state++;
+            break;
+    case 1: 
+            //output low all TRIG pins
+            SENZOR_TRIG_PORT &= ~((1<<SENZOR_FATA_TRIG) | (1<<SENZOR_SPATE_TRIG) | (1<<SENZOR_STANGA_TRIG) | (1<<SENZOR_DREAPTA_TRIG));
+            
+            PCICR  |=   (1<<PCIE2); //enable ISR
+            
+            state++;
+            break;
+            
+    default: state = 0;
+  }  
+}
+
+
+ISR(PCINT2_vect) {
+  
+  static uint8_t lastStatus = 0;
+  unsigned long time = micros();
+  
+  uint8_t changes = lastStatus ^ SENZOR_ECHO_PIN;
+ 
+  if(changes & (1 << SENZOR_FATA_ECHO)) {  //SENZOR_FATA_ECHO was changed
+    if(SENZOR_ECHO_PIN & (1<<SENZOR_FATA_ECHO)) { //pin is now high
+        distante_start_millis[0] = time;
+    }else{
+        distante_end_millis[0] = time;
+    }
+  }
+  
+  if(changes & (1 << SENZOR_SPATE_ECHO)) {
+    if(SENZOR_ECHO_PIN & (1<<SENZOR_SPATE_ECHO)) {
+        distante_start_millis[1] = time;
+    }else{
+        distante_end_millis[1] = time;
+    }
+  }
+  
+  if(changes & (1 << SENZOR_STANGA_ECHO)) {  
+    if(SENZOR_ECHO_PIN & (1<<SENZOR_STANGA_ECHO)) { 
+        distante_start_millis[2] = time;
+    }else{
+        distante_end_millis[2] = time;
+    }
+  }
+  
+  if(changes & (1 << SENZOR_DREAPTA_ECHO)) {
+    if(SENZOR_ECHO_PIN & (1<<SENZOR_DREAPTA_ECHO)) {
+        distante_start_millis[3] = time;
+    }else{
+        distante_end_millis[3] = time;
+    }
+  }
+  
+  lastStatus = SENZOR_ECHO_PIN;
+}
+
