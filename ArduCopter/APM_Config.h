48d5ddb070327a7382d0006e0366b247597ffd7b
claudiucherciu <cherciu_claudiu@yahoo.com>
2014-06-11 17:14:06 +0300

    implements sequential sensors read

--- ArduCopter/APM_Config.h
+++ ArduCopter/APM_Config.h
@@ -23,7 +23,7 @@
  # define CLI_ENABLED           DISABLED            // disable the CLI (command-line-interface) to save 21K of flash space
 #endif
 
-//#define LOGGING_ENABLED       DISABLED            // disable dataflash logging to save 11K of flash space
+#define LOGGING_ENABLED       DISABLED            // disable dataflash logging to save 11K of flash space
 #define MOUNT                 DISABLED            // disable the camera gimbal to save 8K of flash space
 #define AUTOTUNE_ENABLED      DISABLED            // disable the auto tune functionality to save 7k of flash
 #define AC_FENCE              DISABLED            // disable fence to save 2k of flash
--- ArduCopter/Ultrasunete.ino
+++ ArduCopter/Ultrasunete.ino
@@ -16,84 +16,121 @@ void ultrasunete_init() {
   set_output(SENZOR_TRIG_DDR, SENZOR_STANGA_TRIG);
   set_output(SENZOR_TRIG_DDR, SENZOR_DREAPTA_TRIG);
   //output low all TRIG pins
-  SENZOR_TRIG_PORT &= ~((1<<SENZOR_FATA_TRIG) | (1<<SENZOR_SPATE_TRIG) | (1<<SENZOR_STANGA_TRIG) | (1<<SENZOR_DREAPTA_TRIG));
-
-  //enable PCINT for ECHO pins
-  PCMSK2 |= ( (1<<SENZOR_FATA_ECHO) | (1<<SENZOR_SPATE_ECHO) | (1<<SENZOR_STANGA_ECHO) | (1<<SENZOR_DREAPTA_ECHO));
-  //PCICR  |=   (1<<PCIE2); //enable ISR
+  output_low(SENZOR_TRIG_PORT, SENZOR_FATA_TRIG);
+  output_low(SENZOR_TRIG_PORT, SENZOR_SPATE_TRIG);
+  output_low(SENZOR_TRIG_PORT, SENZOR_STANGA_TRIG);
+  output_low(SENZOR_TRIG_PORT, SENZOR_DREAPTA_TRIG);
+  
+  sei();
 }
 
 
 void ultrasunete_50Hz() {
 
   static unsigned int state = 0;
-  switch(state) {
-    case 0:
-            //disable ISR to avoid timing updates
-            PCICR  &=   ~(1<<PCIE2); 
-            
-            //output high all TRIG pins;
-            SENZOR_TRIG_PORT |= ((1<<SENZOR_FATA_TRIG) | (1<<SENZOR_SPATE_TRIG) | (1<<SENZOR_STANGA_TRIG) | (1<<SENZOR_DREAPTA_TRIG));
-            
-            //Compute last distances
-            for(int i = 0; i < 4; i++) {
-              
-              if (distante_end_millis[i] > distante_start_millis) {
-                unsigned long dist = (distante_end_millis[i] - distante_start_millis) / 58;       //sound speed for round-trip travel
+  
+  
+  if(state % 2 == 0) {  //send TRIG on even numbers
                 
-                if ((dist > SENZOR_MIN_RANGE) && (dist < SENZOR_MAX_RANGE)) {
-                  distante[i] = dist;
-                  if(i == 0) {
-                    senzorStart = distante_start_millis;
-                    senzorStop  = distante_end_millis[i];
-                  }
-                }
-              }
-            }
-            
-            state++;
-            break;
-    case 1: 
-            distante_start_millis = micros() + SENZOR_DELAY_RESPONSE;
-            //output low all TRIG pins
-            SENZOR_TRIG_PORT &= ~((1<<SENZOR_FATA_TRIG) | (1<<SENZOR_SPATE_TRIG) | (1<<SENZOR_STANGA_TRIG) | (1<<SENZOR_DREAPTA_TRIG));
-            PCICR  |=   (1<<PCIE2); //enable ISR
-            sei();
-            
-            state++;
-            break;
+    //output high TRIG pin;
+    switch(state/2) {
+      case 0:
+        SENZOR_TRIG_PORT |= (1<<SENZOR_FATA_TRIG);
+        break;
+      case 1:
+        SENZOR_TRIG_PORT |= (1<<SENZOR_SPATE_TRIG);
+        break;
+      case 2:
+        SENZOR_TRIG_PORT |= (1<<SENZOR_STANGA_TRIG);
+        break;
+      case 3:
+        SENZOR_TRIG_PORT |= (1<<SENZOR_DREAPTA_TRIG);
+        break;
+    }
     
-    //wait between pulses
-    case 2:
-    case 3:
-            state++;   
-            break;
-    default: state = 0;
-  }  
+    state++;
+            
+  }else{    //read ECHO on odd numbers
+
+    //mark the ECHO start moment
+    distante_start_millis = micros() + SENZOR_DELAY_RESPONSE;
+    
+    //output low TRIG pin
+    SENZOR_TRIG_PORT &= ~((1<<SENZOR_FATA_TRIG) | (1<<SENZOR_SPATE_TRIG) | (1<<SENZOR_STANGA_TRIG) | (1<<SENZOR_DREAPTA_TRIG));
+    
+    //enable PCINT for ECHO pins
+    switch((state-1) /2) {
+      case 0:
+        PCMSK2 |= (1<<SENZOR_FATA_ECHO);
+        break;
+      case 1:
+        PCMSK2 |= (1<<SENZOR_SPATE_ECHO);
+        break;
+      case 2:
+        PCMSK2 |= (1<<SENZOR_STANGA_ECHO);
+        break;
+      case 3:
+        PCMSK2 |= (1<<SENZOR_DREAPTA_ECHO);
+        break;
+    }
+    
+    PCICR  |= (1<<PCIE2); //enable ISR
+    
+    
+    state++;
+    
+  }
+  
+  if (state > 7) { 
+    state = 0;
+    compute_distances(); //update all sensor distances
+  }
 }
 
 
+void compute_distances() {
+  //Compute last distances
+  for(int i = 0; i < 4; i++) {
+    
+    if (distante_end_millis[i] > distante_start_millis) {
+      unsigned long dist = (distante_end_millis[i] - distante_start_millis) / 58;       //sound speed for round-trip travel
+      
+      if ((dist > SENZOR_MIN_RANGE) && (dist < SENZOR_MAX_RANGE)) {
+        distante[i] = dist;
+        if(i == 0) {
+          senzorStart = distante_start_millis;
+          senzorStop  = distante_end_millis[i];
+        }
+      }
+    }
+  }
+}//compute_distances()
+
 ISR(PCINT2_vect) {
   uint8_t port = SENZOR_ECHO_PIN;
-  sei();  //reenable interrupts, we saved what was critical
+  //sei();  //reenable interrupts, we saved what was critical
   
   unsigned long time = micros();
   
-  //Check if any pin is now LOW, and log the time
+  //Check if any pin is now LOW, and mark the time
   if((port & (1<<SENZOR_FATA_ECHO)) == 0) { 
-      distante_end_millis[0] = time; 
+      distante_end_millis[0] = time;
+      PCMSK2 &= ~(1<<SENZOR_FATA_ECHO); //disable PCINT on the pin
   }
   
-  if((port & (1<<SENZOR_SPATE_ECHO)) == 0) {
+  if((port & (1<<SENZOR_SPATE_ECHO)) == 0) { 
       distante_end_millis[1] = time;
+      PCMSK2 &= ~(1<<SENZOR_SPATE_ECHO);
   }
   
   if((port & (1<<SENZOR_STANGA_ECHO)) == 0) { 
       distante_end_millis[2] = time;
+      PCMSK2 &= ~(1<<SENZOR_STANGA_ECHO);
   }
   
-  if((port & (1<<SENZOR_DREAPTA_ECHO)) == 0) {
+  if((port & (1<<SENZOR_DREAPTA_ECHO)) == 0) { 
       distante_end_millis[3] = time;
+      PCMSK2 &= ~(1<<SENZOR_DREAPTA_ECHO);
   }
   
 }
